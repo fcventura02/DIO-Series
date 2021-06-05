966b26b2362c9f754f2a8441f4faf8d7b09c4bf5
fcventura02 <filipecvr4@gmail.com>
2021-06-05 00:26:48 -0300

    Added serie menu and  serie controller

--- Program.cs
+++ Program.cs
@@ -1,149 +1,47 @@
 ﻿using System;
-using DIO.Series.Classes;
+using DIO.Series.Controller;
 
 namespace DIO.Series
 {
     class Program
     {
-        static SerieRepositorios repositorio = new SerieRepositorios();
-
+        //private SerieController controller = new SerieController();
         static void Main(string[] args)
         {
-            string opcaoUsuario = ObterOpcaoUsuario();
+            string opcaoUsuario = ObterOpcaoMenuUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
             {
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        ListarSerie();
+                        MenuSerie();
                         break;
                     case "2":
-                        Insere();
-                        break;
-                    case "3":
-                        Atualizar();
-                        break;
-                    case "4":
-                        Excluir();
-                        break;
-                    case "5":
-                        RetornaPorId();
+                        MenuFilme();
                         break;
                     case "C":
                         Console.Clear();
                         break;
+                    case "X":
+                        Console.WriteLine("Saindo... Até a proxima!");
+                        break;
                     default: throw new ArgumentException("Opção informada não existe!");
                 }
-                opcaoUsuario = ObterOpcaoUsuario();
+                opcaoUsuario = ObterOpcaoMenuUsuario();
             }
         }
 
-        private static void ListarSerie()
-        {
-            Console.WriteLine("Listar séries");
-            var lista = repositorio.Lista();
 
-            if (lista.Count == 0)
-            {
-                Console.WriteLine("Nemhuma série cadastrada");
-                return;
-            }
-
-            foreach (var item in lista)
-            {
-                Console.WriteLine("#ID {0} - {1} {2}", item.retornaId(), item.retornaTitulo(), item.getExcluido() ? "- Excluido" : "");
-            }
-        }
-        private static void Insere()
-        {
-            Console.WriteLine("Inserir Série");
-            foreach (int item in System.Enum.GetValues(typeof(Enum.Genero)))
-            {
-                Console.WriteLine("{0} - {1}", item, System.Enum.GetName(typeof(Enum.Genero), item));
-            }
-            Console.Write("Digite o gênero entre as opções acima: ");
-            int entradaGenero = int.Parse(Console.ReadLine());
-            Console.Write("Digite o titulo da série: ");
-            string entradaTitulo = Console.ReadLine();
-            Console.Write("Digite o ano de início da série: ");
-            int entradaAno = int.Parse(Console.ReadLine());
-            Console.Write("Digite a descrição da série: ");
-            string entradaDescicao = Console.ReadLine();
-
-            Serie novaSerie = new Serie(id: repositorio.ProximoId(), (Enum.Genero)entradaGenero, entradaTitulo, entradaDescicao, entradaAno);
-            repositorio.Insere(novaSerie);
-        }
-        private static void Excluir()
-        {
-            Console.WriteLine("Excluir Série");
-            Console.WriteLine("Insira o ID da Série para ser excluída");
-            int id = int.Parse(Console.ReadLine());
-            Serie serie = repositorio.RetornaPorId(id);
-            if (serie == null)
-            {
-                Console.WriteLine("Série não encontrada");
-                return;
-            }
-            if (serie.getExcluido())
-            {
-                Console.WriteLine("Série já foi excluída");
-                return;
-            }
-            repositorio.Excluir(id);
-
-        }
-        private static void Atualizar()
-        {
-
-            Console.WriteLine("Atualizar Série");
-            Console.WriteLine("Insira o ID da Série para ser Atualizada");
-            int id = int.Parse(Console.ReadLine());
-            Serie serie = repositorio.RetornaPorId(id);
-            if (serie == null)
-            {
-                Console.WriteLine("Série não encontrada");
-                return;
-            }
-            foreach (int item in System.Enum.GetValues(typeof(Enum.Genero)))
-            {
-                Console.WriteLine("{0} - {1}", item, System.Enum.GetName(typeof(Enum.Genero), item));
-            }
-            Console.Write("Digite o gênero entre as opções acima: ");
-            int entradaGenero = int.Parse(Console.ReadLine());
-            Console.Write("Digite o titulo da série: ");
-            string entradaTitulo = Console.ReadLine();
-            Console.Write("Digite o ano de início da série: ");
-            int entradaAno = int.Parse(Console.ReadLine());
-            Console.Write("Digite a descrição da série: ");
-            string entradaDescicao = Console.ReadLine();
-
-            serie = new Serie(id, (Enum.Genero)entradaGenero, entradaTitulo, entradaDescicao, entradaAno);
-            repositorio.Atualiza(id, serie);
-        }
-        private static void RetornaPorId()
-        {
-            Console.WriteLine("Selecione uma série");
-            Console.WriteLine("Insira o ID da Série para ser apresentada");
-            int id = int.Parse(Console.ReadLine());
-            Serie serie = repositorio.RetornaPorId(id);
-            if (serie == null)
-            {
-                Console.WriteLine("Série não encontrada");
-                return;
-            }
-            Console.WriteLine(serie.ToString());
-
-        }
         private static string ObterOpcaoUsuario()
         {
             Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1- Listar séries");
-            Console.WriteLine("2- Inserir nova série");
-            Console.WriteLine("3- Atualizar série");
-            Console.WriteLine("4- Excluir série");
-            Console.WriteLine("5- Vizualizar série");
+            Console.WriteLine("1- Listar");
+            Console.WriteLine("2- Inserir");
+            Console.WriteLine("3- Atualizar");
+            Console.WriteLine("4- Excluir");
+            Console.WriteLine("5- Vizualizar");
             Console.WriteLine("C- Limpar tela");
             Console.WriteLine("X- Sair");
 
@@ -151,6 +49,87 @@ namespace DIO.Series
 
             Console.WriteLine();
             return opcaoUsuario;
+        }
+        private static string ObterOpcaoMenuUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Menu Série");
+            Console.WriteLine("2- Menu Filme");
+            Console.WriteLine("X- Sair");
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+        private static void MenuSerie()
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                Console.WriteLine("Menu Série");
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        SerieController.ListarSerie();
+                        break;
+                    case "2":
+                        SerieController.Insere();
+                        break;
+                    case "3":
+                        SerieController.Atualizar();
+                        break;
+                    case "4":
+                        SerieController.Excluir();
+                        break;
+                    case "5":
+                        SerieController.RetornaPorId();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    case "X":
+                        Console.WriteLine("Voltando...");
+                        break;
+                    default: throw new ArgumentException("Opção informada não existe!");
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+        private static void MenuFilme()
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                Console.WriteLine("Menu Série");
+               /* switch (opcaoUsuario)
+                {
+                    case "1":
+                        SerieController.ListarSerie();
+                        break;
+                    case "2":
+                        SerieController.Insere();
+                        break;
+                    case "3":
+                        SerieController.Atualizar();
+                        break;
+                    case "4":
+                        SerieController.Excluir();
+                        break;
+                    case "5":
+                        SerieController.RetornaPorId();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    case "X":
+                        Console.WriteLine("Voltando...");
+                        break;
+                    default: throw new ArgumentException("Opção informada não existe!");
+                }*/
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
         }
     }
 }
